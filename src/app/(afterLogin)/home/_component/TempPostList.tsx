a20cf53ec1c85f05988a3a2854cd563c4eee73ab
jkea1 <jinkyng7890@gmail.com>
2024-01-04 00:05:52 +0900

    feat: 임시 포스트 리스트 생성

---
+++ src/app/(afterLogin)/home/_component/TempPostList.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useContext } from 'react'
+import { TabContext } from './TabProvider'
+
+export default function TempPostList() {
+  const { tab } = useContext(TabContext)
+
+  console.log('tabState', tab)
+
+  return (
+    <>
+      {tab === 'rec' && (
+        <>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+          <li>추천 포스트</li>
+        </>
+      )}
+      {tab === 'fol' && (
+        <>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+          <li>팔로우 포스트</li>
+        </>
+      )}
+    </>
+  )
+}
