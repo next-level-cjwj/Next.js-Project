060171ce16a4255cdfe2944c9228fb7a7bbc181d
jkea1 <jinkyng7890@gmail.com>
2024-01-24 12:30:40 +0900

    feat: 기존 회원 재 회원가입 시도시 에러 상황 가정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -17,6 +17,7 @@ export const handlers = [
       }
     )
   }),
+
   http.post('/api/logout', () => {
     return new HttpResponse(null, {
       headers: {
@@ -24,19 +25,21 @@ export const handlers = [
       },
     })
   }),
+
   http.post('/api/users', async () => {
     console.log('회원가입')
     // msw 로 흉내를 낼 수 있다.
+
     // 동일한 id의 user가 회원가입 했을경우 403 에러를 보내준다.
-    return HttpResponse.text(JSON.stringify('user_exists'), {
-      status: 403,
-    })
+    // return HttpResponse.text(JSON.stringify('user_exists'), {
+    //   status: 403,
+    // })
 
     // 회원가입 성공하는 경우
-    // return HttpResponse.text(JSON.stringify("ok"), {
-    //   headers: {
-    //     'Set-Cookie' : "connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0"
-    //   }
-    // })
+    return HttpResponse.text(JSON.stringify('ok'), {
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0',
+      },
+    })
   }),
 ]
