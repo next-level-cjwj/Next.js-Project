acd3439c3239eb8ba32f4fec99fb33a3ce695d1c
jkea1 <jinkyng7890@gmail.com>
2024-01-24 12:29:22 +0900

    chore: 주석 수정

--- src/app/_component/MSWComponent.tsx
+++ src/app/_component/MSWComponent.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export const MSWComponent = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // 클라이언트 환경(브라우져)을 말한다. 아래의 코드가 브라우저에서 돌아간다는 것을 한버 더 보장한다.
+      // 클라이언트 환경(브라우져)을 말한다. 아래의 코드가 브라우저에서 돌아간다는 것을 한번 더 보장한다.
       if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
         require('@/mocks/browser')
       }
